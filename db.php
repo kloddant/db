08e6be4bc03121d6bd3a9e63ee90a53c82997ba5
Tony <kloddant@users.noreply.github.com>
2016-05-20 09:01:24 -0500

    Created the sql_result class
    Separated the sql class into the sql class and the sql_results class, which extends the sql class.  The sql class now represents a connection and contains the querying methods.  The sql_results class represents a database resultset and contains the methods to traverse this resultset.

--- db.php
+++ db.php
@@ -3,11 +3,17 @@
 class sql {
 
 	public $connection;
-	private $row = array();
-	private $stmt;
 
 	public function __construct($host = NULL, $username = NULL, $password = NULL, $database = NULL) {
 		$this->connect($host, $username, $password, $database);
+	}
+
+	public function __destruct() {
+		$this->connection->close();
+	}
+
+	protected static function convert_to_reference(&$value) {
+		return $value;
 	}
 
 	public function connect($host = "localhost", $username = "root", $password = NULL, $database = NULL) {
@@ -21,7 +27,7 @@ class sql {
 
 		$connection = new mysqli($host, $username, $password, $database);
 		if (!$connection) {
-			exit("Error: Could not connect to the database.");
+			exit("We're sorry, but we can't connect to the database at the moment.  Please try again later.");
 		}
 
 		$connection->set_charset("utf8");
@@ -30,20 +36,14 @@ class sql {
 		return $connection;
 	}
 
-	public function prepare($sql, $connection = NULL) {
+	public function prepare($sql) {
 
-		if (!isset($connection)) {
-			$connection = $this->connection;
-		}
+		$connection = $this->connection;
 		if (!($stmt = $connection->prepare($sql))) {
 		    echo "Prepare failed: (" . $connection->errno . ") " . $connection->error;
 		}
 		return $stmt;
 
-	}
-
-	private static function convert_to_reference(&$value) {
-		return $value;
 	}
 
 	public function execute($stmt, $parameters = array(), $types = '') {
@@ -75,15 +75,16 @@ class sql {
 
 		// Execute the statement.
 		if (!$stmt->execute()) {
-		    echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+		    exit("Execute failed: (" . $stmt->errno . ") " . $stmt->error);
 		}
 		// If the statement was a SELECT query, then create the empty bound parameters array.
-		else if ($stmt->affected_rows < 0) {
+		$row = "";
+		if ($stmt->affected_rows < 0) {
 			$stmt->store_result();
 			$fields = $stmt->result_metadata()->fetch_fields();
 			$params = array();
 			$duplicates = array();
-		    foreach ($fields as $field) {
+		    foreach ($fields as &$field) {
 		        $key = str_replace(' ', '_', $field->name);
 		        if (!array_key_exists($key, $duplicates)) {
 		        	$duplicates[$key] = 0;
@@ -97,26 +98,65 @@ class sql {
 	        	}
 		    }
 		    call_user_func_array(array($stmt, 'bind_result'), $params);
-		    $this->row = $params;
+		    $row = $params;
 		}
-		$this->stmt = $stmt;
-		return $stmt;
+
+		$sql_result = new sql_result($stmt, $row);
+		return $sql_result;
 
 	}
 
-	public function query($sql, $parameters = array(), $types = '', $buffer=true, $connection = NULL) {
+	public function query($sql, $parameters = array(), $types = '', $buffer=true, $transpose = false) {
 	
-		if (!isset($connection)) {
-			$connection = $this->connection;
-		}
+		$connection = $this->connection;
 		$stmt = $this->prepare($sql, $connection);
 		$executed_stmt = $this->execute($stmt, $parameters, $types);
 		if ($buffer) {
-			return $this->fetch_all("MYSQLI_ASSOC");
+			return $executed_stmt->fetch_all("MYSQLI_ASSOC", $transpose);
 		}
 		else {
-			return $this;
+			return $executed_stmt;
 		}
+	}
+
+	public function change_user($username, $password, $database) {
+		if (!isset($password)) {
+			exit('Error: No database password defined.');
+		}
+		if (!isset($database)) {
+			exit('Error: No database defined.');
+		}
+		return $this->connection->change_user($username, $password, $database);
+	}
+
+	public function insert_id() {
+		return $this->connection->insert_id;
+	}
+
+}
+
+
+
+class sql_result extends sql {
+
+	private $row = array();
+	private $stmt;
+
+	public function __construct($stmt, $row) {
+		$this->stmt = $stmt;
+		$this->row = $row;
+	}
+
+	public function __destruct() {
+		$this->free();
+	}
+
+	public function fetch_row() {
+		return ($this->stmt->fetch() ? array_values(array_map("sql_result::convert_to_reference", $this->row)) : false);
+	}
+
+	public function fetch_assoc() {
+		return ($this->stmt->fetch() ? array_map("sql_result::convert_to_reference", $this->row) : false);
 	}
 
 	public function fetch_array($resulttype = "MYSQLI_BOTH") {
@@ -131,26 +171,8 @@ class sql {
 		}
 	}
 
-	public function fetch_row() {
-		return ($this->stmt->fetch() ? array_values($this->row) : false);
-	}
-
-	public function fetch_assoc() {
-		return ($this->stmt->fetch() ? $this->row : false);
-	}
-
 	public function affected_rows() {
 		return $this->stmt->affected_rows;
-	}
-
-	public function change_user($username, $password, $database) {
-		if (!isset($password)) {
-			exit('Error: No database password defined.');
-		}
-		if (!isset($database)) {
-			exit('Error: No database defined.');
-		}
-		return $this->connection->change_user($username, $password, $database);
 	}
 
 	public function data_seek($offset = 0) {
@@ -161,19 +183,11 @@ class sql {
 		return $this->stmt->field_count;
 	}
 
-	public function insert_id() {
-		return $this->connection->insert_id;
-	}
-
 	public function num_rows() {
 		return $this->stmt->num_rows;
 	}
 
-	public function get_result() {
-		return $this;
-	}
-
-	public function fetch_all($resulttype = "MYSQLI_NUM") {
+	public function fetch_all($resulttype = "MYSQLI_NUM", $transpose = false) {
 		$results = array();
 		if ($resulttype == "MYSQLI_BOTH") {
 			while ($result = $this->fetch_array("MYSQLI_BOTH")) {
@@ -190,7 +204,24 @@ class sql {
 				$results[] = $result;
 			}
 		}
+		if ($transpose) {
+			$results = $this->transpose($results);
+		}
 		return $results;
+	}
+
+	private static function transpose($array) {
+		$transposed = array();
+		foreach ($array as $row => $values) {
+			foreach ($values as $column => $cell) {
+				$transposed[$column][$row] = $cell;
+			}
+		}
+		return $transposed;
+	}
+
+	public function free() {
+		$this->stmt->free_result();
 	}
 
 }
